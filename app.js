3ef2f17bd3f89bca4671e039224a751220aad7ee
Sud <sudarsana.ala@gmail.com>
2022-10-12 16:47:42 +0530

    added books endpoint

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const os = require('os')
+
+const books = require('./data/books.json');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send(`Hello World! - ${os.hostname()}`)
+})
+
+app.get('/books', (req, res) => {  
+  res.send(`${JSON.stringify(books)}`);
 })
 
 app.listen(port, () => {
